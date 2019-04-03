8b4360fd1e9f9adc7fb7c4bf61285c6c5bee6f6d
Chris Harling <cehxc5@mail.missouri.edu>
2019-04-03 12:58:23 -0500

    Added color struct and set background colors to color table

---
+++ colorsWithNavigation/colorsWithNavigation/Color.swift
@@ -0,0 +1,16 @@
+//
+//  Color.swift
+//  colorsWithNavigation
+//
+//  Created by Chris Harling on 4/3/19.
+//  Copyright © 2019 Chris Harling. All rights reserved.
+//
+
+import UIKit
+
+struct Color{
+    let name: String
+    let uiColor: UIColor
+    
+}
+
